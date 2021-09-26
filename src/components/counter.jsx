ea8d142550c8a76a5395c1dd85ebfe55a862568b
Octavio Lafourcade <octaviolafourcade@MacBook-Pro-de-Octavio.local>
2021-09-26 17:37:50 -0500

    [ ADD ] Count to State for implementing dynamically [ UPDATE ] Span for showing state data using this.state.count

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 class Counter extends React.Component {
+    state = {
+        count: 0
+    };
     render() { 
         return (
             <React.Fragment>
-                <h1>Hello World!</h1>
+                <span>{this.state.count}</span>
                 <button>Increment</button>
             </React.Fragment>
         );
