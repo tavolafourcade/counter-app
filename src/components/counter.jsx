f69316d33a69ba288c966b57fc21fadb0d8e97ce
Octavio Lafourcade <octaviolafourcade@MacBook-Pro-de-Octavio.local>
2021-09-26 17:08:16 -0500

    [ ADD ] Counter Component [ UPDATE ] Import Counter component in index.js

---
+++ src/components/counter.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+class Counter extends React.Component {
+    render() { 
+        return <div>
+            <h1>Hello World!</h1>
+        </div>;
+    }
+}
+ 
+export default Counter;
