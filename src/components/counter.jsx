55900d77fcf54972189b51f2407df80e783990b5
Octavio Lafourcade <octaviolafourcade@MacBook-Pro-de-Octavio.local>
2021-09-26 19:49:14 -0500

    [ UPDATE ] Implemented getBadgeClasses as separate method

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -4,14 +4,18 @@ class Counter extends React.Component {
         count: 0
     };
     render() { 
-        let classes ='badge m-2 badge-';
-        classes += (this.state.count === 0) ? 'warning' : 'primary';
         return (
             <div className='container'>
-                <span className={classes}>{this.formatCount()}</span>
+                <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
                 <button className='btn btn-secondary btn-sm'>Increment</button>
             </div>
         );
+    }
+
+    getBadgeClasses() {
+        let classes = 'badge m-2 badge-';
+        classes += (this.state.count === 0) ? 'warning' : 'primary';
+        return classes;
     }
 
     formatCount(){
